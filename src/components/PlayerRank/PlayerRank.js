32410c1b525ad9803b31c503d1470512d0216c7f
Matthew Lancellotti <mvlancellotti@gmail.com>
2020-10-14 10:36:03 -0400

    clean names for display

--- src/components/PlayerRank/PlayerRank.js
+++ src/components/PlayerRank/PlayerRank.js
@@ -16,7 +16,7 @@ function PlayerRank() {
 	// set state
   const [stat, setStat] = useState('aces')
   const [limit, setLimit] = useState(5)
-  const [data, setData] = useState([('', 0)])
+  const [data, setData] = useState([{category:'', value1:0}])
   // create handlers
   const handleChangeStat = (event) => {
     setStat(event.target.value)
--- src/components/PlayerRankChart/PlayerRankChart.js
+++ src/components/PlayerRankChart/PlayerRankChart.js
@@ -3,13 +3,23 @@ import PropTypes from 'prop-types'
 
 import RadialBarChart from '../RadialBarChart'
 
+const cleanName = (name) => {
+  return name.replace(/[_]/, ' ')
+}
 
 function PlayerRankChart(props) {
+  // clean data a bit
+  const data = props.data.map((datum) => {
+    return {
+      ...datum,
+      'category': cleanName(datum.category),
+    }
+  })
   // return rendered stuff
 	return (
     <RadialBarChart {...{
       onClick: props.fetchData,
-  		data: props.data,
+  		data,
     }}/>
 	);
 }
